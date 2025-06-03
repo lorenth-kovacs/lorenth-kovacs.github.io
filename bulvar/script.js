5b7932fd2790e0b32768e4b34f254b90a3720ed7
Kovács Ádám Szt2 <kovacs.adam.24f@szlgbp.hu>
2025-06-03 10:00:51 +0200

    first commit

---
+++ bulvar/script.js
@@ -0,0 +1,15 @@
+const body = document.getElementById('body');
+
+document.addEventListener('mousemove', (e) => {
+  const rect = body.getBoundingClientRect();
+  const scrollTop = body.scrollTop;
+  const scrollLeft = body.scrollLeft;
+
+  const x = e.clientX - rect.left + scrollLeft;
+  const y = e.clientY - rect.top + scrollTop;
+
+  const percentX = (x / body.scrollWidth) * 100;
+  const percentY = (y / body.scrollHeight) * 100;
+
+  body.style.background = `radial-gradient(circle at ${percentX}% ${percentY}%, black 0%, red 3%, blue 6%, black 15%)`;
+});
