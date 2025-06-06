db7b62192a047372379cdc0df10cae2a96a884c8
Kovács Ádám Szt2 <kovacs.adam.24f@szlgbp.hu>
2025-06-06 09:48:16 +0200

    kurzor

--- bulvar/script.js
+++ bulvar/script.js
@@ -1,4 +1,5 @@
 const body = document.getElementById('body');
+const kurzor = document.querySelector("#kurzor");
 
 document.addEventListener('mousemove', (e) => {
   const rect = body.getBoundingClientRect();
@@ -12,5 +13,9 @@ document.addEventListener('mousemove', (e) => {
   const percentY = (y / body.scrollHeight) * 100;
 
   body.style.background = `radial-gradient(circle at ${percentX}% ${percentY}%, black 0%, red 3%, blue 6%, black 15%)`;
+
+  kurzor.style.top = e.clientY - (kurzor.clientHeight / 2);
+  kurzor.style.left = e.clientX - (kurzor.clientWidth / 2);
+  console.log("kurzor mozog");
 });
 
